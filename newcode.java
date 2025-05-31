c3a6e5328408c283eec7e81f1494641163794c4b
Josuesilva2003 <josuesilvabaltodano075@gmail.com>
2025-05-31 16:27:23 -0600

    New code en proceso, terminar lecciones y codigo

---
+++ newcode.java
@@ -0,0 +1,55 @@
+public class newcode {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        
+
+int j = 0;
+do {
+   System.out.println(j);
+ j++;
+}
+while (j < 9);
+
+
+int countDonw = 3; 
+
+while (countDonw > 0) {
+    System.out.println(countDonw);
+countDonw--;
+}
+       
+System.out.println("Happy new yeard");
+        
+int Dice = 1; 
+
+while (Dice <= 6) {
+    if (Dice < 6){
+       System.out.println("No Yatzy"); 
+    }else {
+        System.out.println("Yes Yatzy");
+    }
+    Dice = Dice + 1; 
+}
+
+
+for (int q = 0; q < 5; q++ ){
+    System.out.println(q);
+}
+
+
+for ( int k = 0; k <= 10; k = k +2){
+    System.out.println(k);
+}
+
+
+
+}
+
+
+
+
+}
