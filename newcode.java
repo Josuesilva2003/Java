636ca2a6018684b868dbf90fd626cbbed52decd5
Josuesilva2003 <josuesilvabaltodano075@gmail.com>
2025-06-01 00:48:56 -0600

    New code Ter,ime el proyecto en esa hoja

--- newcode.java
+++ newcode.java
@@ -44,12 +44,93 @@ for (int q = 0; q < 5; q++ ){
 for ( int k = 0; k <= 10; k = k +2){
     System.out.println(k);
 }
+ 
 
+ for (int a = 1; a <= 2; a++ ){
+    System.out.println("Outer: " + a);
+ }
 
+ for ( int ñ = 1; ñ <= 2; ñ++){
+    System.out.println("inter: " + ñ);
+ }
+ String[] cars ={"volvo", "BMW", "Mazda" };
+ for (String c: cars){
+    System.out.println(c);
+ }
 
+for (int z = 0; z <= 100; z += 10){
+    System.out.println(z);
+}
+
+    
+for (int p = 0; p <= 10; p = p +2){
+System.out.println(p);
 }
 
 
 
 
+int numb = 2;
+
+for (int g = 1; g <= 10; g++ ){
+    System.out.println(numb + "x" + g + "=" + (numb * g));
+}
+
+
+for ( int r = 0; r < 10; r++){
+    if (r == 4) {
+        break;
+    }
+    System.out.println(r);
+}
+
+for ( int y = 0; y < 10; y++){
+    if (y == 4) {
+        continue;
+    }
+    System.out.println(y);
+}
+
+
+int o = 0;
+while (o < 10){
+    System.out.println(o);
+
+    o ++;
+if (o == 4 ){
+    break;
+}
+
+}
+
+   
+
+int u = 0;
+  while(u < 10){
+   if (u ==4){
+ u++;
+    continue;
+   }
+   
+System.out.println(u);
+u++;
+    }
+
+
+String[] auto = {"volvo", "Bmw", "Mazda", "audi"} ;
+auto[0] = "opel"; 
+System.out.println(auto[0]);
+
+
+//para ver cuantos elementos tiene en una matriz usar length
+
+String[] auro = {"volvo", "bmw", "ford", "mazda"};
+System.out.println(auro.length);
+// va a decir cuantos elementos tengo q son 4
+
+
+
+
+
+    }
 }
